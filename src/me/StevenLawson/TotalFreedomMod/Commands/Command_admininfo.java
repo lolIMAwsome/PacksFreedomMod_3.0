9bc9d9346ce1751f29c585c2a2062d1809ff513b
PacksGamingHD <craftyswans@gmail.com>
2014-07-27 21:29:48 -0700

    Fix website bug

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -12,8 +12,8 @@ public class Command_admininfo extends TFM_Command {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
 
-        playerMsg(ChatColor.AQUA + "The following is accurate as of 05/14/14");
-        playerMsg(ChatColor.AQUA + "To apply for admin you need to go to the forums at http:/packsfreedom.icyboards.net");
+        playerMsg(ChatColor.AQUA + "The following is accurate as of 07/27/14");
+        playerMsg(ChatColor.AQUA + "To apply for admin you need to go to the forums at http://packsfreedom.icyboards.net");
         playerMsg(ChatColor.AQUA + "Read the template!");
         playerMsg(ChatColor.AQUA + "Make a new thread called My Super Admin Application");
         playerMsg(ChatColor.RED + "The PacksFreedom server highly recommends that you do not ask for reccomendations for admin. It will decrease your chances of becoming a Super Admin");
